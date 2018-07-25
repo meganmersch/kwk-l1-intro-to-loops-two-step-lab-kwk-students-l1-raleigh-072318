dfc2c226519e84ea6bbab1b8c26b0e0d89432f58
meganmersch <meganrm8@gmail.com>
2018-07-25 14:24:27 +0000

    automatically backed up by learn

--- two_step_and_repeat.rb
+++ two_step_and_repeat.rb
@@ -39,8 +39,20 @@ end
   # Write a loop that outputs the first two sets of steps in the Two-Step
 
 def how_many_steps?
-  # Write a loop that fulfills all criteria from Part 3 of the README.md
+  steps = 0 
+  loop do 
+    steps += 1 
+    puts steps 
+  if steps % 2 ==0 
+    puts "Left"
+  else 
+    puts "Right"
+  end
+  sleep (0.5)
 end
+
+  
+  # Write a loop that fulfills all criteria from Part 3 of the README.md
 
 def break_dance
   # Write a solution that uses the same code as how_many_steps?, but breaks the
